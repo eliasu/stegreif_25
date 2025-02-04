89fe97eb9a711c990911e502e14a253f8687b5be
Eliasu <elias@commandg.de>
2025-02-04 17:53:19 +0100

    updates

--- storage/framework/cache/facade-be6d8504f9b789d1425501b26f7813b644815514.php
+++ storage/framework/cache/facade-be6d8504f9b789d1425501b26f7813b644815514.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace Facades\Statamic\Structures;
+namespace Facades\Statamic\UpdateScripts;
 
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @see \Statamic\Structures\CollectionTreeDiff
+ * @see \Statamic\UpdateScripts\Manager
  */
-class CollectionTreeDiff extends Facade
+class Manager extends Facade
 {
     /**
      * Get the registered name of the component.
      */
     protected static function getFacadeAccessor(): string
     {
-        return 'Statamic\Structures\CollectionTreeDiff';
+        return 'Statamic\UpdateScripts\Manager';
     }
 }
