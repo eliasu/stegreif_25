63a57472af3b331a4802f60784cdce851e53ba75
Eliasu <elias@commandg.de>
2025-02-06 15:06:07 +0100

    changed gitignores on storage / removed storage cache files from git

--- storage/framework/views/0ae0660e71d2427f8200883137bdcd3a.php
+++
@@ -1,8 +0,0 @@
-<session-expiry
-    email="<?php echo e($email); ?>"
-    :warn-at="<?php echo e($warnAt); ?>"
-    :lifetime="<?php echo e($lifetime); ?>"
-    :oauth-provider="<?php echo e(json_encode($oauth)); ?>"
-    :auth="<?php echo e(json_encode($auth)); ?>"
-></session-expiry>
-<?php /**PATH /Users/elias/Websites/stegreif_25/vendor/statamic/cms/src/Providers/../../resources/views/partials/session-expiry.blade.php ENDPATH**/ ?>
